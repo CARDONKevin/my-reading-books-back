2f05d49f9c2166521ce9113ef965b58ff1e2e479
CARDON <cardonkevin02@gmail.com>
2019-03-25 14:20:37 +0100

    Add login Facebook

--- src/main/java/com/insset/ccm/kevincardon/myreadingbooksback/persistence/dao/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.insset.ccm.kevincardon.myreadingbooksback.persistence.dao;
-
-import com.insset.ccm.kevincardon.myreadingbooksback.persistence.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-        User findByUsername(final String username);
-}
