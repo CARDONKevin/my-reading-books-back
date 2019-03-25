333a9bae8f7e3129241f2daa29785c64cb1bbe7a
CARDONKevin <34343838+CARDONKevin@users.noreply.github.com>
2019-03-25 11:06:34 +0100

    Sf social login facebook (#2)
    * Prepare Auth by Facebook with spring social facebook
    
    * Add login template managed by Facebook API

--- src/main/java/com/insset/ccm/kevincardon/myreadingbooksback/MyReadingBooksBackApplication.java
+++ src/main/java/com/insset/ccm/kevincardon/myreadingbooksback/MyReadingBooksBackApplication.java
@@ -2,9 +2,15 @@ package com.insset.ccm.kevincardon.myreadingbooksback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
-public class MyReadingBooksBackApplication {
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+@EnableJpaRepositories("com.insset.ccm.kevincardon.myreadingbooksback.persistence.dao")
+@EntityScan("com.insset.ccm.kevincardon.myreadingbooksback.persistence.model")
+public class MyReadingBooksBackApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyReadingBooksBackApplication.class, args);
