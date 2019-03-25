1a219674ee9b461f430088aa903b211c936aeb20
CARDON <cardonkevin02@gmail.com>
2019-03-25 14:48:30 +0100

    Add functionnal login facebook

--- src/main/java/com/insset/ccm/kevincardon/myreadingbooksback/MyReadingBooksBackApplication.java
+++ src/main/java/com/insset/ccm/kevincardon/myreadingbooksback/MyReadingBooksBackApplication.java
@@ -3,12 +3,9 @@ package com.insset.ccm.kevincardon.myreadingbooksback;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
-@EnableJpaRepositories("com.insset.ccm.kevincardon.myreadingbooksback.persistence.dao")
+@SpringBootApplication
 @EntityScan("com.insset.ccm.kevincardon.myreadingbooksback.persistence.model")
 public class MyReadingBooksBackApplication extends SpringBootServletInitializer {
 
--- src/main/java/com/insset/ccm/kevincardon/myreadingbooksback/controllers/IndexController.java
+++ src/main/java/com/insset/ccm/kevincardon/myreadingbooksback/controllers/IndexController.java
@@ -1,16 +1,41 @@
 package com.insset.ccm.kevincardon.myreadingbooksback.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.social.connect.ConnectionRepository;
+import org.springframework.social.facebook.api.Facebook;
+import org.springframework.social.facebook.api.PagedList;
+import org.springframework.social.facebook.api.Post;
+import org.springframework.social.facebook.api.User;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @RestController
 public class IndexController {
 
-    @RestController
-    public class ProductController {
-        @GetMapping(value="/")
-        public String IndexApp() {
-            return "Welcome in backend of MyReadingsBooks";
+    private Facebook facebook;
+    private ConnectionRepository connectionRepository;
+
+    public IndexController(Facebook facebook, ConnectionRepository connectionRepository) {
+        this.facebook = facebook;
+        this.connectionRepository = connectionRepository;
+    }
+
+    @GetMapping(value = "/")
+    public ModelAndView IndexApp() {
+        if (connectionRepository.findPrimaryConnection(Facebook.class) == null) {
+            return new ModelAndView("redirect:/connect/facebook");
         }
+        return new ModelAndView("redirect:https://my-reading-books-dev.herokuapp.com/");
+    }
+
+    @DeleteMapping(value = "/connect/facebook")
+    public ModelAndView Logout() {
+        if (connectionRepository.findPrimaryConnection(Facebook.class) == null) {
+            connectionRepository.removeConnection(connectionRepository.findPrimaryConnection(Facebook.class).getKey());
+        }
+        return new ModelAndView("redirect:/");
     }
 }
+
